1ded205d4ef86967f61d5c1e129cfffc107c0e8e
Vadim_Lyutsko <vadim_lyutsko@mail.ru>
2023-09-20 19:25:49 +0300

    Improved mobile layout and fixed minor errors in text and color. The project is completely ready

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,37 @@
-// import mobileNav from './modules/mobile-nav.js';
-// mobileNav();
+import mobileNav from './modules/mobile-nav.js';
+import loader from './modules/loader.js'
 
-// import Swiper bundle with all modules installed
+mobileNav();
+loader();
+
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
 
 // init Swiper:
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    loop: true,
-    parallax: true,
-    speed: 1000,
+	// Optional parameters
+	loop: true,
+	parallax: true,
+	speed: 1000,
 
-    keyboard: {
-        enabled: true,
-    },
+	keyboard: {
+		enabled: true,
+	},
 
-    // If we need pagination
-    pagination: {
-        el: '.slider-controls__count',
-        type: 'fraction',
-    },
+	// If we need pagination
+	pagination: {
+		el: '.slider-controls__count',
+		type: 'fraction',
+	},
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '#sliderNext',
-        prevEl: '#sliderPrev',
-    },
+	// Navigation arrows
+	navigation: {
+		nextEl: '#sliderNext',
+		prevEl: '#sliderPrev',
+	},
 
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
+	// And if we need scrollbar
+	scrollbar: {
+		el: '.swiper-scrollbar',
+	},
 });
